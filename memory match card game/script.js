fd906a25d521b74c1c5c49cc44e0b028c8a4275e
saramaher1 <sara.almaher94@gmail.com>
2022-10-10 11:50:37 +0200

    User experience (UX)

--- memory match card game/script.js
+++ memory match card game/script.js
@@ -20,12 +20,59 @@ function shuffleCards() {
 }
 shuffleCards();
 function flipCard(evt) {
-  const clickedCard = evt.target;
-  if (!cardOne) {
-    // if there is not yet a value assigned to the cardOne variable...
-    return (cardOne = clickedCard); // set the cardOne value as the clickedCard and end this function.
+  // take an event object's as a scoped variable
+  const clickedCard = evt.target; // set the event's target DOM element as a variable
+  if (cardOne !== clickedCard && !disableDeck) {
+    // make sure that the current variable cardOne is not the same value as the clickedCard, AND that the deck is NOT disabled
+    clickedCard.classList.add("flip"); // add the 'flip' class to the classes currently assigned to the clickedCard
+    if (!cardOne) {
+      // if there is not yet a value assigned to the cardOne variable...
+      return (cardOne = clickedCard); // set the cardOne value as the clickedCard and end this function.
+    }
+    // everything below will execute if the condition above was not met (if cardOne already had a value when flipCard() was called)
+    cardTwo = clickedCard; // set the cardTwo value as the clickedCard
+    disableDeck = true; // set this to true for the next time this flipCard function is called, when the top level condition is evaluated
+    // if the function has come this far, it means we have set values for both cardOne and cardTwo.
+    // each of the cardOne and cardTwo variables currently represent a whole HTML element with childNodes
+    let cardOneImg = cardOne.querySelector(".back-view img").src; // query the elements inside cardOne to get the value of the img src, such as `img-2.png`, and set that as the value of cardOneImg
+    let cardTwoImg = cardTwo.querySelector(".back-view img").src; // query the elements inside cardOne to get the value of the img src, such as `img-2.png`, and set that as the value of cardTwoImg
+    matchCards(cardOneImg, cardTwoImg); // now check the images by filename to see if they are a match!
   }
-  // everything below will execute if the condition above was not met (if cardOne already had a value when flipCard() was called)
-  cardTwo = clickedCard; // set the cardTwo value as the clickedCard
-  disableDeck = true; // set this to true for the next time this flipCard function is called, when the top level condition is evaluated
 }
+
+function matchCards(img1, img2) {
+  if (img1 === img2) {
+    // this code will run if the card images match
+    matchedPairs++; // if the card images match, we can increment the global `matchedPairs` variable by 1 match
+    if (matchedPairs == 8) {
+      // if your number of matches is 8, you've made all the matches! Game Won!
+      console.log("YOU WIN!");
+      return; // for now, lets call this game over, end this function and do nothing else.
+    }
+    // everything below will execute if the game has not yet been won...
+    cardOne.removeEventListener("click", flipCard); // remove the eventlistener so that this matched card cannot be flipped anymore
+    cardTwo.removeEventListener("click", flipCard); // remove the eventlistener so that this matched card cannot be flipped anymore
+    cardOne = cardTwo = ""; // now reset the cardOne & cardTwo variables to empty strings, so we can use them again
+    disableDeck = false;
+    return; // end function
+  }
+  // these cards didn't match, un-flip them...
+  cardOne.classList.remove("flip");
+  cardTwo.classList.remove("flip");
+  cardOne = cardTwo = ""; // reset the cardOne & cardTwo variables to empty string
+  disableDeck = false;
+  return;
+}
+setTimeout(() => {
+  cardOne.classList.remove("flip");
+  cardTwo.classList.remove("flip");
+  cardOne = cardTwo = ""; // reset the cardOne & cardTwo variables to empty string
+  disableDeck = false;
+  return;
+}, 1200);
+setTimeout(() => {
+  cardOne.classList.add("shake");
+  cardTwo.classList.add("shake");
+}, 400);
+cardOne.classList.remove("shake", "flip");
+cardTwo.classList.remove("shake", "flip");
