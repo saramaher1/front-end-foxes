2b3fa6f9dd9a94dd03845bcb1554af07280a7822
saramaher1 <sara.almaher94@gmail.com>
2022-10-18 10:27:58 +0200

    added two components gameBoard and CardView , and add the static style into main.js

---
+++ memory-game-vue/src/components/CardView.vue
@@ -0,0 +1,11 @@
+<script>
+import CardView from "./CardView.vue";
+
+export default {
+  components: {
+    CardView,
+  },
+};
+</script>
+
+<template></template>
---
+++ memory-game-vue/src/components/GameBoard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="game-board">
+    <p>I am the GameBoard.vue component</p>
+    <ul class="cards">
+      <li class="card">
+        <CardView />
+      </li>
+    </ul>
+  </div>
+</template>
