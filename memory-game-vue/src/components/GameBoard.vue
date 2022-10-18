638a34d8230774af0e3834989e60e53a614dc1d7
saramaher1 <sara.almaher94@gmail.com>
2022-10-18 10:53:40 +0200

    added the v-for and the card data

--- memory-game-vue/src/components/GameBoard.vue
+++ memory-game-vue/src/components/GameBoard.vue
@@ -1,16 +1,23 @@
 <script>
 import CardView from "./CardView.vue";
+import cardData from "../data/memoryCards8.js";
 
 export default {
   components: {
     CardView,
   },
+  data() {
+    return {
+      cardsData: cardData.concat(cardData),
+    };
+  },
 };
 </script>
+
 <template>
   <div class="game-board">
     <ul class="cards">
-      <li>
+      <li v-for="(cardInfo, index) in cardsData" :key="index" class="card">
         <CardView viewType="front" />
         <CardView
           viewType="back"
